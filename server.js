412236a656387710d0e4bc37c30fb30133d61e28
AlfieriChou <alfierichou@gmail.com>
2020-10-02 15:07:32 +0800

    refactor: rpc server

--- server.js
+++ server.js
@@ -1,16 +1,36 @@
 const grpc = require('grpc')
+const path = require('path')
+const readDirFilenames = require('read-dir-filenames')
 
-const message = require('./app/model/message_grpc_pb')
-const Message = require('./app/service/message')
+const classLoader = require('./app/lib/classLoader')
+
+const { PORT = 50051 } = process.env
 
 const server = new grpc.Server()
 
-server.addService(message.MessageServiceService, new Message())
+const run = async () => {
+  const service = classLoader(path.join(__dirname, '/app/service'))
+  const protoFilepaths = readDirFilenames(path.join(__dirname, '/app/proto'))
 
-server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), (err) => {
-  if (err) {
-    throw err
-  }
-  console.log('Server running at http://0.0.0.0:50051')
-  server.start()
-})
+  await protoFilepaths.reduce(async (promise, filepath) => {
+    await promise
+    const modelName = path.basename(filepath).replace(/\.\w+$/, '')
+    // eslint-disable-next-line import/no-dynamic-require, global-require
+    const model = require(path.join(__dirname, `/app/model/${modelName}_grpc_pb`))
+    Object.entries(model).forEach(([handlersName, handlers]) => {
+      if (handlersName.endsWith('Service')) {
+        server.addService(handlers, service[`${modelName}`])
+      }
+    })
+  }, Promise.resolve())
+
+  server.bindAsync(`0.0.0.0:${PORT}`, grpc.ServerCredentials.createInsecure(), (err) => {
+    if (err) {
+      throw err
+    }
+    console.log(`Server running at tcp://0.0.0.0:${PORT}`)
+    server.start()
+  })
+}
+
+run()
