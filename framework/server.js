db697b44a1a4d7f7a5cd503d9b6b707238ead058
AlfieriChou <alfierichou@gmail.com>
2020-10-23 17:25:01 +0800

    refactor: initializeServer loadDescriptors

--- framework/server.js
+++ framework/server.js
@@ -5,6 +5,47 @@ const classLoader = require('./lib/classLoader')
 const { loadProtoFilePaths } = require('./common')
 const loadPb = require('./pb')
 
+const loadDescriptors = ({ modelName, service, pb }) => {
+  const descriptorNames = Object
+    .keys(Object.getOwnPropertyDescriptors(service[`${modelName}`]))
+    .filter(name => name !== 'constructor')
+  return descriptorNames.reduce((acc, descriptorName) => {
+    const descriptor = service[`${modelName}`][descriptorName]
+    if (!(typeof descriptor === 'function')) {
+      throw new Error(`/app/service/${modelName} ${descriptorName} descriptor must be function`)
+    }
+    let func
+    if (descriptor.constructor.name === 'AsyncFunction') {
+      func = async (call, callback) => {
+        // eslint-disable-next-line no-param-reassign
+        call.pb = pb
+        try {
+          const result = await service[`${modelName}`][descriptorName](call)
+          callback(null, result)
+        } catch (err) {
+          callback(err, null)
+        }
+      }
+    }
+    if (descriptor.constructor.name === 'Function') {
+      func = (call, callback) => {
+        // eslint-disable-next-line no-param-reassign
+        call.pb = pb
+        try {
+          const result = service[`${modelName}`][descriptorName](call)
+          callback(null, result)
+        } catch (err) {
+          callback(err, null)
+        }
+      }
+    }
+    return {
+      ...acc,
+      [descriptorName]: func
+    }
+  }, {})
+}
+
 module.exports = async ({
   port = 50051,
   insecure = grpc.ServerCredentials.createInsecure(),
@@ -22,44 +63,7 @@ module.exports = async ({
     const model = require(path.join(process.cwd(), `/app/model/${modelName}_grpc_pb`))
     Object.entries(model).forEach(([handlersName, handlers]) => {
       if (handlersName.endsWith('Service')) {
-        const descriptorNames = Object
-          .keys(Object.getOwnPropertyDescriptors(service[`${modelName}`]))
-          .filter(name => name !== 'constructor')
-        const descriptors = descriptorNames.reduce((acc, descriptorName) => {
-          const descriptor = service[`${modelName}`][descriptorName]
-          if (!(typeof descriptor === 'function')) {
-            throw new Error(`/app/service/${modelName} ${descriptorName} descriptor must be function`)
-          }
-          let func
-          if (descriptor.constructor.name === 'AsyncFunction') {
-            func = async (call, callback) => {
-              // eslint-disable-next-line no-param-reassign
-              call.pb = pb
-              try {
-                const result = await service[`${modelName}`][descriptorName](call)
-                callback(null, result)
-              } catch (err) {
-                callback(err, null)
-              }
-            }
-          }
-          if (descriptor.constructor.name === 'Function') {
-            func = (call, callback) => {
-              // eslint-disable-next-line no-param-reassign
-              call.pb = pb
-              try {
-                const result = service[`${modelName}`][descriptorName](call)
-                callback(null, result)
-              } catch (err) {
-                callback(err, null)
-              }
-            }
-          }
-          return {
-            ...acc,
-            [descriptorName]: func
-          }
-        }, {})
+        const descriptors = loadDescriptors({ modelName, service, pb })
         server.addService(handlers, descriptors)
       }
     })
